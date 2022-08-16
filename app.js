228d953d0a2535e2c578cec3538fee4eeb6c0291
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-16 03:26:10 +0200

    remove

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express  = require('express')
 const paypal = require('paypal-rest-sdk')
 const app = express()
 
+app.use('view engine', 'ejs')
+
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>console.log(`App's running`))
