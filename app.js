f786b73d721ea8f0f671345b20099165de1ec55e
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-08-16 04:14:47 +0200

    execute payments route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 
 const express = require("express");
 const ejs = require("ejs");
+const paypal = require("paypal-rest-sdk");
 
 // paypal configuration
 paypal.configure({
@@ -33,24 +34,79 @@ app.post("/pay", (req, res) => {
             {
               name: "Best Italian shoes",
               sku: "001",
-              price: "32.00",
+              price: "64.00",
               currency: "USD",
-              quantity: 1,
+              quantity: 2,
             },
           ],
         },
         amount: {
           currency: "USD",
-          total: "64.00",
+          total: "128.00",
         },
         description: "Shoes for formal and ceremonial activities.",
       },
     ],
   };
 
+  try {
+    paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+        throw error;
+      } else {
+        // console.log(payment);
+        const { links } = payment;
 
-   
+        for (const linkObj of links) {
+          if (linkObj.rel === "approval_url") {
+            res.redirect(linkObj.href);
+          }
+        }
+      }
+    });
+  } catch (err) {
+    console.log(`something wrong happened`);
+  }
 });
+
+app.get("/success", (req, res) => {
+//   console.log(req.query); // for testing
+  const { PayerID } = req.query;
+  const { paymentId } = req.query;
+
+  //   execute payments
+  const execute_payment_json = {
+    payer_id: PayerID,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: "128.00",
+        },
+      },
+    ],
+  };
+
+//   paypal.payment.execute(
+//     paymentId,
+//     execute_payment_json,
+//     function (error, payment) {
+//       if (error) {
+//         console.log(error.response);
+//         throw error;
+//       } else {
+//         console.log(payment);
+//         // to be put in the database later
+//         console.log(JSON.stringify(payment));
+//         res.send("Success");
+//       }
+//     }
+//   );
+});
+
+// app.get("/cancel", (req, res) => {
+//   res.send("Payment process cancelled");
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App's running`));
